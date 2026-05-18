a68ebfe46ffd13118eb9383ecf1361e09a658921
urrutiamateo <urrutiamateo7@gmail.com>
2026-05-18 16:57:38 -0300

    add Base

--- script/EnemyBase.gd
+++ script/EnemyBase.gd
@@ -8,16 +8,13 @@ var vida_actual
 var path_follow : PathFollow2D
 
 func _ready() -> void:
+	add_to_group("enemigos")
 	vida_actual = vida_max
 
 func _process(delta: float) -> void:
 	if path_follow != null:
 		path_follow.progress += velocidad * delta
 		global_position = path_follow.global_position
-
-		var path_length = path_follow.get_parent().curve.get_baked_length()
-		if path_follow.progress >= path_length - 1.0: # margen de 1 pixel
-			alcanzar_nexo()
 
 func take_damage(cantidad):
 	vida_actual -= cantidad
@@ -26,8 +23,4 @@ func take_damage(cantidad):
 		die()
 
 func die():
-	queue_free()
-
-func alcanzar_nexo():
-	print("Enemy alcanzo el nexo")
 	queue_free()
